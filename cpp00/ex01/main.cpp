70c14f2419752eca23163645b2f2501040561e5c
tglory <tglory@student.42Lyon.fr>
2021-12-28 05:43:02 +0100

    starting cpp00/ex01

---
+++ cpp00/ex01/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tglory <tglory@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/28 05:33:22 by tglory            #+#    #+#             */
+/*   Updated: 2021/12/28 05:41:43 by tglory           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "phonebook.hpp"
+
+int main(void)
+{
+	std::string input;
+
+	std::cout << "ADD, SEARCH or EXIT" << std::endl;
+	std::cin >> input;
+	std::cout << "Choosing '" << input << "' ..." << std::endl;
+	 
+	return (0);
+}
