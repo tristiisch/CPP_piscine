683041605e5730b5ed1f6b3a67614a62f04babcf
tglory <tglory@student.42Lyon.fr>
2021-12-28 04:54:27 +0100

    remove ptr

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: tglory <tglory@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 04:24:13 by tglory            #+#    #+#             */
-/*   Updated: 2021/12/28 04:38:21 by tglory           ###   ########lyon.fr   */
+/*   Updated: 2021/12/28 04:54:08 by tglory           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main(int argc, char *argv[])
 {
 	int		i;
-	char	*c;
+	int		j;
 
 	if (argc == 1)
 	{
@@ -25,13 +25,13 @@ int main(int argc, char *argv[])
 	i = 1;
 	while (argv[i])
 	{
-		c = argv[i];
-		while (*c)
+		j = 0;
+		while (argv[i][j])
 		{
-			*c = toupper(*c);
-			c++;
+			//if (argv[i][j] != ' ' || (j > 0 && argv[i][j] == ' ' && argv[i][j - 1] != ' '))
+			std::cout << (char)toupper(argv[i][j]);
+			j++;
 		}
-		std::cout << argv[i];
 		if (argv[i + 1])
 			std::cout << " ";
 		else
