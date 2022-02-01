cc8495f1a33fca512dfadff59d59b057d996e98d
tglory <tglory@student.42Lyon.fr>
2022-02-02 00:01:44 +0100

    fix compile error

--- cpp04/ex03/ICharacter.hpp
+++ cpp04/ex03/ICharacter.hpp
@@ -17,6 +17,7 @@ class ICharacter
 		virtual void equip(AMateria* m) = 0;
 		virtual void unequip(int idx) = 0;
 		virtual void use(int idx, ICharacter& target) = 0;
+		virtual void unEquipSafe(int idx) = 0;
 };
 
 #endif
