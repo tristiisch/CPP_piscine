b12edfa4cf397e3ceea9c82865be9bc2e1b16d98
tglory <tglory@student.42Lyon.fr>
2022-02-04 03:46:35 +0100

    add cpp06/ex01 & cpp06/ex02 to test it

---
+++ cpp06/ex01/Data.hpp
@@ -0,0 +1,11 @@
+#ifndef DATA_HPP
+# define DATA_HPP
+
+# include <iostream>
+
+typedef struct	s_Data
+{
+	int nbr;
+} Data;
+
+#endif
---
+++ cpp06/ex01/main.cpp
@@ -0,0 +1,26 @@
+# include "Data.hpp"
+
+uintptr_t serialize(Data* ptr)
+{
+	return (reinterpret_cast<uintptr_t>(ptr));
+}
+
+Data* deserialize(uintptr_t raw)
+{
+	return (reinterpret_cast<Data*>(raw));
+}
+
+int		main(void)
+{
+	Data *ptr = new Data;
+
+	ptr->nbr = 10;
+	std::cout << "Address of ptr:\t\t\t\t" << ptr << "\nValue in ptr struct:\t\t\t" << ptr->nbr << std::endl;
+	uintptr_t tmp_serialize = serialize(ptr);
+	std::cout << "Address after serialize() use:\t\t" << tmp_serialize << std::endl;
+	Data *recovered = deserialize(tmp_serialize);
+	std::cout << "Address after deserialize() use:\t" << recovered << std::endl;
+	std::cout << "Value after deserialize() use:\t\t" << recovered->nbr << std::endl;
+	delete ptr;
+	return (0);
+}
