7666920e6b31668436396622aca6dfe33b7a7510
tglory <tglory@student.42Lyon.fr>
2022-02-07 15:48:22 +0100

    up tests main cpp08/ex02

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tglory <tglory@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 08:54:55 by tglory            #+#    #+#             */
-/*   Updated: 2022/02/07 01:01:49 by tglory           ###   ########lyon.fr   */
+/*   Updated: 2022/02/07 15:27:07 by tglory           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,14 @@ int main()
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
+	std::cout << "Top : " << mstack.top() << std::endl;
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+	std::cout << "Size : " <<  mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
 	mstack.push(0);
+	std::cout << "Size : " <<  mstack.size() << std::endl;
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 	++it;
@@ -33,6 +34,19 @@ int main()
 		std::cout << *it << std::endl;
 		++it;
 	}
+	std::cout << std::endl;
+
+	std::cout << "Reverse :" << std::endl;
 	std::stack<int> s(mstack);
+	std::cout << "Size : " <<  s.size() << std::endl;
+	MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+	MutantStack<int>::reverse_iterator rite = mstack.rend();
+	++rit;
+	--rit;
+	while (rit != rite)
+	{
+		std::cout << *rit << std::endl;
+		++rit;
+	}
 	return 0;
 }
